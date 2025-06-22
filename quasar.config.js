8d4e6cc403e8f8056e7748d4924825a7c172644c
Stefan Luy <sluy1283@gmail.com>
2025-06-22 10:33:46 -0400

    ???

--- quasar.config.js
+++ quasar.config.js
@@ -45,14 +45,14 @@ module.exports = configure(function (/* ctx */) {
         node: 'node20',
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/pinata/',
+      //publicPath: '/pinata/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
